bfa8ed070adf856b0911fdd53a94af1e54a37226
gadfly <szcshui@gmail.com>
2011-09-02 08:32:51 -0700

    add test message
    Change-Id: Icbab17e0e7a366516635e2d73d69ed6cc2dafd18

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,5 +1,5 @@
 package hellotest
 
 object Main {
-  def main(args : Array[String]) = println("hello scalatest eclipse")
+  def main(args : Array[String]) = println("hello scalatest eclipse, welcome!")
 }
