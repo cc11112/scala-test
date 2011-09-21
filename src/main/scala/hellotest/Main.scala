6c6e2a74b6ff560a56afe99fed308d85389f6731
Crane <szcshui@gmail.com>
2011-09-21 18:21:45 -0500

    use Nat

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,6 +1,28 @@
 package hellotest
 
+abstract class Nat
+
+case object Zero extends Nat
+
+case class Succ(n: Nat) extends Nat {
+  def toInt(n: Nat): Int = n match {
+    case Zero => 0
+    case Succ(m) => 1 + toInt(m)
+
+    case _ => error("can not convert to integer")
+  }
+
+  def plus(n: Nat)(m: Nat): Nat = n match {
+    case Zero => m
+    case Succ(k: Nat) => plus(k)(Succ(m))
+  }
+}
+
+
 //change code here
 object Main {
-  def main(args : Array[String]) = println("hello scalatest eclipse, welcome!")
+  val a = new Succ(Zero);
+  val b = new Succ(a);
+  
+  def main(args: Array[String]) = println(a.toInt(b.plus(a)(b)))
 }
